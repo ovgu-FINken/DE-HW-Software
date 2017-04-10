49979c6d86032c7db5092aa62f3916d33648f54d
Serhii <serhii.vasylchenko@gmail.com>
2017-04-10 21:02:50 +0200

    updated LEDStrip class

--- Nucleo-F411RE-FINken/drivers/LEDStrip/LEDStrip.cpp
+++ Nucleo-F411RE-FINken/drivers/LEDStrip/LEDStrip.cpp
@@ -1,36 +1,30 @@
-//#include "LEDStrip.h"
-//#include "WS8212.h"
-//#include "PixelArray.h"
-//
-//LEDStrip::LEDStrip(PinName pin, int size, int zeroHigh, int zeroLow, int oneHigh, int oneLow): ws(pin, size, zeroHigh, zeroLow, oneHigh, oneLow), px(size) {
-//
-//    ws.useII(WS2812::PER_PIXEL); // use per-pixel intensity scaling
-//    stripSize = size;
-//    int r_offset = 0;
-//    int g_offset = 0;
-//    int b_offset = 0;
-//
-//    /*
-//    // set up the colours we want to draw with
-//    int colorbuf[NUM_COLORS] = {0x2f0000,0x2f2f00,0x002f00,0x002f2f,0x00002f,0x2f002f};
-//
-//    // for each of the colours (j) write out 10 of them
-//    // the pixels are written at the colour*10, plus the colour position
-//    // all modulus 60 so it wraps around
-//    for (int i = 0; i < WS2812_BUF; i++) {
-//        px.Set(i, colorbuf[(i / NUM_LEDS_PER_COLOR) % NUM_COLORS]);
-//    }
-//
-//    // now all the colours are computed, add a fade effect using intensity scaling
-//    // compute and write the II value for each pixel
-//    for (int j=0; j<WS2812_BUF; j++) {
-//        // px.SetI(pixel position, II value)
-//        px.SetI(j, 0xf+(0xf*(j%NUM_LEDS_PER_COLOR)));
-//    }
-//    */
-//
-//}
-//
-//void LEDStrip::update() {
-//    ws.write(px.getBuf());
-//}
+#include "LEDStrip.h"
+#include "WS2812.h"
+#include "PixelArray.h"
+
+LEDStrip::LEDStrip(PinName pin, int size, int zeroHigh, int zeroLow, int oneHigh, int oneLow): ws(pin, size, zeroHigh, zeroLow, oneHigh, oneLow), px(size) {
+
+    ws.useII(WS2812::GLOBAL); // use per-pixel intensity scaling
+    stripSize = size;
+    r_offset = 0;
+    g_offset = 0;
+    b_offset = 0;
+    mode = 0;
+
+}
+
+void LEDStrip::update() {
+    switch (mode) {
+        case 0:
+            // Control from Paparazzi
+            break;
+        case 1:
+            // Running red
+            px.Set(0, 0xFF0000);
+            ws.write_offsets(px.getBuf(), r_offset, g_offset, b_offset);
+            r_offset += 1 % stripSize;
+            g_offset += 1 % stripSize;
+            b_offset += 1 % stripSize;
+            break;
+    }
+}
--- Nucleo-F411RE-FINken/drivers/LEDStrip/LEDStrip.h
+++ Nucleo-F411RE-FINken/drivers/LEDStrip/LEDStrip.h
@@ -1,21 +1,40 @@
-//#pragma once
-//
-//#include "../AbstractComponent.h"
-//#include "mbed.h"
-//#include "PinNames.h"
-//#include "WS8212.h"
-//#include "PixelArray.h"
-//
-//class LEDStrip: public AbstractComponent {
-//public:
-//    LEDStrip(PinName pin, int size, int zeroHigh, int zeroLow, int oneHigh, int oneLow);
-//    virtual void update();
-//
-//private:
-//    WS2812 ws;
-//    PixelArray px;
-//    int stripSize;
-//    int r_offset;
-//    int g_offset;
-//    int b_offset;
-//};
+#pragma once
+
+#include "../AbstractComponent.h"
+#include "mbed.h"
+#include "PinNames.h"
+#include "WS2812.h"
+#include "PixelArray.h"
+
+class LEDStrip : public AbstractComponent {
+public:
+     /**
+      * Constructor for creating a new LED Strip
+      *
+      * @param pin Output pin. Connect to "Din" on the first WS2812 in the strip
+      * @param size Number of LEDs in your strip
+      * @param zeroHigh How many NOPs to insert to ensure TOH is properly generated. See library description for more information.
+      * @param zeroLow How many NOPs to insert to ensure TOL is properly generated. See library description for more information.
+      * @param oneHigh How many NOPs to insert to ensure T1H is properly generated. See library description for more information.
+      * @param oneLow How many NOPs to insert to ensure T1L is properly generated. See library description for more information.
+      */
+    LEDStrip(PinName pin, int size, int zeroHigh, int zeroLow, int oneHigh, int oneLow);
+
+    /**
+     * Set LED Strip to manual control instead of getting control action from paparazzi, which is set by default
+     *
+     * @param mode one of predifined modes
+     */
+    void setMode(uint8_t mode);
+
+    virtual void update();
+
+private:
+    WS2812 ws;
+    PixelArray px;
+    int stripSize;
+    int r_offset;
+    int g_offset;
+    int b_offset;
+    uint8_t mode;
+};
