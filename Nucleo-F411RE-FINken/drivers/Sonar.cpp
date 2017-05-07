70ea9d202b187e321961f7a82b9c374eb1abf6e4
shrutisub <shruti.subramanian>
2017-05-07 16:59:46 -0700

    Sonar changes

--- Nucleo-F411RE-FINken/drivers/Sonar.cpp
+++ Nucleo-F411RE-FINken/drivers/Sonar.cpp
@@ -2,31 +2,49 @@
 #include <stdint.h>
 #include "IRQLock.h"
 
+    //char cmd[2];
+    //while (1) {
+//        cmd[0] = 0x01;
+//        cmd[1] = 0x00;
+//        i2c.write(addr, cmd, 2);
+//
+//        wait(0.5);
+//
+//        cmd[0] = 0x00;
+//        i2c.write(addr, cmd, 1);
+//        i2c.read(addr, cmd, 2);
+//
+//        float tmp = (float((cmd[0]<<8)|cmd[1]) / 256.0);
+//        printf("Temp = %.2f\n", tmp);
+//    }
+//}
+
 Sonar::Sonar(UARTMessenger *const uartMsngr, uint8_t addr): uartMessenger(uartMsngr), i2c(I2C_SDA, I2C_SCL) {
     id = ++s_id;
-    address = addr << 1;
-    range = 0;
+    const int address = (0x70 << 1);
+    int range;
+    char config[2];
 
-    config[0] = 81; // Take a range reading, 81 = 0x51
-//    config_r[1] = 0;
 }
 
 void Sonar::update() {
     i2c.lock();
-    int a = i2c.write(address, config, 1);
-    if (a != 0)
-        return; //Writing failed
+    config[0] = 0x51;//initalizing the address as 81
+    config[1]=0x00; //set the data register to 0
+    i2c.write(address, config, 2);
     timeout.attach(callback(this, &Sonar::read), 0.08); // 80 ms is suggested in documentation
+    config[0]=0x00;
+    i2c.write(address,config,1);
     i2c.unlock();
 }
 
 void Sonar::read() {
     i2c.lock();
     char range_read[2];
-    int a = i2c.read(address | 0x1, range_read, 2); //read the two-byte range data
+    int a = i2c.read(address | 0x01,range_read,2);
     if (a != 0)
         return; // Reading failed
-    range = ((range_read[0] << 8) + range_read[1]);
+    range = ((range_read[0] << 8)|range_read[1]);
     i2c.unlock();
 
     subMessage.type = SONAR;
